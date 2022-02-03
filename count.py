a2bf5254a6e14f374a1d3f958b0eea7d9912f34e
newnithisak <newnithisak@gmail.com>
2022-02-04 01:24:28 +0700

    first commit

---
+++ count.py
@@ -0,0 +1,22 @@
+import datetime
+x = datetime.datetime.now()
+print('########## Receipt ##########')
+j = int(input('NumProduct= '))
+print('########## price ##########')
+sum=0
+t=0
+for i in range(j):
+    h= float(input('price= '))
+    t+=h
+print('########## discount ##########')
+n= int(input('disPer= '))
+p = t*(n/100) #ผมรวม%
+sum = t-p #ผลรวมหลังลบ%
+# sum = t-(t*(n/100))
+print('##########> TotalSummary <##########')
+print('')
+print('Date:',x.strftime("%A"),x.date(),'Time:',x.strftime("%H"),':',x.strftime("%M"))
+print('Summary=',t,'discount--',n,'--%=',p,'bath')
+print('TotalSummary=[',sum,']/Bath')
+print('')
+print('##########> TotalSummary <##########')
---
+++ demo.py
@@ -0,0 +1,9 @@
+print('Nithisak Sangkum')
+print('KMITL')
+print('Computer Science')
+
+for i in range(10):
+    print('Number =' , i+1)
+
+s='Peter'
+print(s.lower())
